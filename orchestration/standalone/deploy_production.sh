405272dbb672954c253fa131fc6cc025c549f4d5
Tanay Nagjee <tanay.nagjee@gmail.com>
2014-06-08 17:02:18 -0700

    add wget to wait for web app

--- orchestration/standalone/deploy_production.sh
+++ orchestration/standalone/deploy_production.sh
@@ -6,8 +6,7 @@ docker ps|grep '$target_port->8080'|awk '{ print "docker rm -f "$1"" }'|sh
 cd /opt/jpetstore/$build_id/docker
 docker build -t nikhilv/jpetstore-$target_port .
 docker run -p $target_port:8080 nikhilv/jpetstore-$target_port &
-
-# Wait until the web application is ready
-
+sleep 2
+wget -t 10 localhost:$target_port/jpetstore/actions/Catalog.action
 etcdctl set ecdemo/upstream $target_port
 confd -node 'http://127.0.0.1:4001' -onetime -confdir /etc/confd
